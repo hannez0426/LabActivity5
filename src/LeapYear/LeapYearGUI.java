7339b9df969bed20385cfb5401920ba3926f9e15
Zennah A <zeenkyy02@gmail.com>
2022-12-17 00:25:23 +0800

    Rename LeapYear components Add LeapYear main function Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -3,6 +3,8 @@ package LeapYear;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.NumberFormatException;
+import java.lang.*;
 
 public class LeapYearGUI extends JFrame{
     private JPanel panel1;
@@ -12,25 +14,25 @@ public class LeapYearGUI extends JFrame{
     public LeapYearGUI(){
         
         btnCheckerYear.addActionListener(new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                double year = Double.parseDouble(tfYear.getText());
-                boolean isleap = false;
-
-                if (year % 4 == 0) {
-                    if (year % 100 == 0) {
-                        if (year % 400 == 0) {
+                    double year = Double.parseDouble(tfYear.getText());
+                    boolean isleap = false;
+                        if (year % 4 == 0) {
+                            if (year % 100 == 0) {
+                                if (year % 400 == 0) {
+                                    isleap = true;
+                                }
+                            }
                             isleap = true;
                         }
-                    }
-                    isleap = true;
-                }
 
-               if(isleap == true){
-                   JOptionPane.showMessageDialog(panel1, "Leap Year");
-               }else{
-                   JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
-                }
+                        if (isleap == true) {
+                            JOptionPane.showMessageDialog(panel1, "Leap Year");
+                        } else {
+                            JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
+                        }
             }
         });
     }
@@ -43,6 +45,7 @@ public class LeapYearGUI extends JFrame{
         JButton another = new JButton("Another");
         leap.setVisible(true);
         leap.setTitle("Leap Year Checker");
+
     }
 }
 
