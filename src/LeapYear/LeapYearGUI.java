a0f6c4d05963419f1064bcaa49f6da87fd776525
Zennah A <zeenkyy02@gmail.com>
2022-12-16 23:34:12 +0800

    Rename LeapYear components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,49 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckerYear;
+
+    public LeapYearGUI(){
+        
+        btnCheckerYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double year = Double.parseDouble(tfYear.getText());
+                boolean isleap = false;
+
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
+                            isleap = true;
+                        }
+                    }
+                    isleap = true;
+                }
+
+               if(isleap == true){
+                   JOptionPane.showMessageDialog(panel1, "Leap Year");
+               }else{
+                   JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        LeapYearGUI leap = new LeapYearGUI();
+        leap.setContentPane(leap.panel1);
+        leap.setSize(500, 500);
+        leap.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JButton another = new JButton("Another");
+        leap.setVisible(true);
+        leap.setTitle("Leap Year Checker");
+    }
 }
+
+
